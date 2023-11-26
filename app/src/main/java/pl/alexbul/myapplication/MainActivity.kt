c0219c91eae3fce1b3b12219319b40e5f5675863
Raccoon <gygel@tut.by>
2023-11-26 15:34:19 +0300

    third

--- app/src/main/java/pl/alexbul/myapplication/MainActivity.kt
+++ app/src/main/java/pl/alexbul/myapplication/MainActivity.kt
@@ -3,13 +3,13 @@ package pl.alexbul.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import pl.alexbul.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,13 +31,19 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MyApplicationTheme {
-        Greeting("Android")
+    Column(modifier = Modifier.fillMaxSize()) {
+
+
+        Box(modifier = Modifier
+            .fillMaxHeight(0.5f)
+            .fillMaxWidth(),
+            contentAlignment = Alignment.Center
+        )
+        {
+
+            Text(text = "Hello $name!")
+
+        }
     }
 }
